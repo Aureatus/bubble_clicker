46ae661a08816ee418ce82a1e90c5000cab0e8a4
Aureatus <93552500+Aureatus@users.noreply.github.com>
2024-09-09 08:05:07 +0100

    Format app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,72 +16,73 @@
 //
 
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
-import "phoenix_html"
+import "phoenix_html";
 // Establish Phoenix Socket and LiveView configuration.
-import {Socket} from "phoenix"
-import {LiveSocket} from "phoenix_live_view"
-import topbar from "../vendor/topbar"
+import { Socket } from "phoenix";
+import { LiveSocket } from "phoenix_live_view";
+import topbar from "../vendor/topbar";
 
-let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
+let csrfToken = document
+  .querySelector("meta[name='csrf-token']")
+  .getAttribute("content");
 
 /**
  * @type {Object.<string, import("phoenix_live_view").ViewHook>}
  */
-let Hooks = {}
+let Hooks = {};
 Hooks.Canvas = {
   mounted() {
     let canvas = this.el;
     let context = canvas.getContext("2d");
 
-    this.pushEvent('Canvas:init', {}, (reply) => {
-        const {data, cell_size} = reply
-        for (const cell of data) {
-          const {value, x, y} = cell
-          const fill = value ? "black" : "blue"
-          context.fillStyle = fill
+    this.pushEvent("Canvas:init", {}, (reply) => {
+      const { data, cell_size } = reply;
+      for (const cell of data) {
+        const { value, x, y } = cell;
+        const fill = value ? "black" : "blue";
+        context.fillStyle = fill;
 
-          context.fillRect(x, y, cell_size, cell_size)
-        }
-    })
+        context.fillRect(x, y, cell_size, cell_size);
+      }
+    });
 
-    this.handleEvent('Canvas:update', (reply) => {
-      const {data, cell_size} = reply
-      const {value, x, y} = data
+    this.handleEvent("Canvas:update", (reply) => {
+      const { data, cell_size } = reply;
+      const { value, x, y } = data;
 
-      const fill = value ? "black" : "blue"
-      context.fillStyle = fill
+      const fill = value ? "black" : "blue";
+      context.fillStyle = fill;
 
-      context.fillRect(x, y, cell_size, cell_size)
-    })
+      context.fillRect(x, y, cell_size, cell_size);
+    });
   },
-}
+};
 
 let liveSocket = new LiveSocket("/live", Socket, {
   longPollFallbackMs: 2500,
-  params: {_csrf_token: csrfToken},
+  params: { _csrf_token: csrfToken },
   metadata: {
     click: (e) => {
       return {
         altKey: e.altKey,
         offsetX: e.offsetX,
-        offsetY: e.offsetY
-      }
-    }
+        offsetY: e.offsetY,
+      };
+    },
   },
-  hooks: Hooks
-})
+  hooks: Hooks,
+});
 
 // Show progress bar on live navigation and form submits
-topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
-window.addEventListener("phx:page-loading-start", _info => topbar.show(300))
-window.addEventListener("phx:page-loading-stop", _info => topbar.hide())
+topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" });
+window.addEventListener("phx:page-loading-start", (_info) => topbar.show(300));
+window.addEventListener("phx:page-loading-stop", (_info) => topbar.hide());
 
 // connect if there are any LiveViews on the page
-liveSocket.connect()
+liveSocket.connect();
 
 // expose liveSocket on window for web console debug logs and latency simulation:
 // >> liveSocket.enableDebug()
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim()
-window.liveSocket = liveSocket
-
+window.liveSocket = liveSocket;
