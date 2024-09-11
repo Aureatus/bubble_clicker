6d6825bf19237adc29470701c0241ed8c5399d81
Alex <93552500+Aureatus@users.noreply.github.com>
2024-09-11 19:42:36 +0100

    Use canvas for bubbles (#4)
    * Set hardcoded width and height, and calculated sizes for bubble cells
    
    * Create bubbles_grid canvas that mirrors div canvas status
    
    * Add ability to pop bubbles via canvas grid
    
    * Remove legacy bubble grid system
    
    * Propagate grid dimensions from server
    
    * Small refactor, calculate values on server when possible
    
    * Compute x and y coordinates on server side
    
    * Add cell_size to assigns
    
    * Format app.js
    
    * Simplify grid model and centralise grid generating function
    
    * Increase time till longpollFallback on LiveSocket
    
    * Update bubbles on update event
    
    * Implement ability to change grid size
    
    * Update colours of bubble grid
    
    * Move bubble business logic to context file

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,29 +16,76 @@
 //
 
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
-import "phoenix_html"
+import "phoenix_html";
 // Establish Phoenix Socket and LiveView configuration.
-import {Socket} from "phoenix"
-import {LiveSocket} from "phoenix_live_view"
-import topbar from "../vendor/topbar"
+import { Socket } from "phoenix";
+import { LiveSocket } from "phoenix_live_view";
+import topbar from "../vendor/topbar";
 
-let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
+let csrfToken = document
+  .querySelector("meta[name='csrf-token']")
+  .getAttribute("content");
+
+/**
+ * @type {Object.<string, import("phoenix_live_view").ViewHook>}
+ */
+let Hooks = {};
+Hooks.Canvas = {
+  mounted() {
+    let canvas = this.el;
+    let context = canvas.getContext("2d");
+
+    this.handleEvent("Canvas:init", (reply) => {
+      context.fillStyle = "#FAF9F6";
+      context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+
+      const { data, cell_size } = reply;
+      for (const cell of data) {
+        const { value, x, y } = cell;
+        const fill = value ? "black" : "#FAF9F6";
+        context.fillStyle = fill;
+
+        context.fillRect(x, y, cell_size, cell_size);
+      }
+    });
+
+    this.handleEvent("Canvas:update", (reply) => {
+      const { data, cell_size } = reply;
+      const { value, x, y } = data;
+
+      const fill = value ? "black" : "#FAF9F6";
+      context.fillStyle = fill;
+
+      context.fillRect(x, y, cell_size, cell_size);
+    });
+  },
+};
+
 let liveSocket = new LiveSocket("/live", Socket, {
-  longPollFallbackMs: 2500,
-  params: {_csrf_token: csrfToken}
-})
+  longPollFallbackMs: 5000,
+  params: { _csrf_token: csrfToken },
+  metadata: {
+    click: (e) => {
+      return {
+        altKey: e.altKey,
+        offsetX: e.offsetX,
+        offsetY: e.offsetY,
+      };
+    },
+  },
+  hooks: Hooks,
+});
 
 // Show progress bar on live navigation and form submits
-topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
-window.addEventListener("phx:page-loading-start", _info => topbar.show(300))
-window.addEventListener("phx:page-loading-stop", _info => topbar.hide())
+topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" });
+window.addEventListener("phx:page-loading-start", (_info) => topbar.show(300));
+window.addEventListener("phx:page-loading-stop", (_info) => topbar.hide());
 
 // connect if there are any LiveViews on the page
-liveSocket.connect()
+liveSocket.connect();
 
 // expose liveSocket on window for web console debug logs and latency simulation:
 // >> liveSocket.enableDebug()
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim()
-window.liveSocket = liveSocket
-
+window.liveSocket = liveSocket;
--- lib/bubble_clicker_web/live/bubble_grid_live/index.ex
+++ lib/bubble_clicker_web/live/bubble_grid_live/index.ex
@@ -1,58 +1,67 @@
 defmodule BubbleClickerWeb.BubbleGridLive.Index do
+  alias BubbleClicker.Bubbles
   use BubbleClickerWeb, :live_view
 
   def mount(_params, _session, socket) do
-    grid_size = 200
-
-    bubbles = generate_bubbles(grid_size)
-    bubbles_grid = generate_grid_from_bubbles_v2(bubbles)
+    grid_size = 20
+    grid_dimension = 800
+    cell_size = Bubbles.calculate_cell_size(grid_dimension, grid_size)
+    bubbles_grid = Bubbles.generate_bubbles_grid(grid_size, cell_size)
 
     socket =
-      assign(socket, :bubbles, bubbles)
+      assign(socket, :bubbles, bubbles_grid)
       |> assign(:grid_size, grid_size)
-      |> stream(:bubbles_grid, bubbles_grid)
+      |> assign(:grid_dimension, grid_dimension)
+      |> assign(:cell_size, cell_size)
 
-    {:ok, socket}
+    {:ok,
+     socket
+     |> push_event("Canvas:init", %{
+       data: bubbles_grid,
+       cell_size: cell_size
+     })}
   end
 
-  def handle_event("pop", %{"id" => id, "column" => column, "row" => row}, socket) do
-    row_number = String.to_integer(row)
-    column_number = String.to_integer(column)
+  def handle_event(
+        "canvas_click",
+        %{"offsetX" => offsetX, "offsetY" => offsetY},
+        socket
+      ) do
+    cell_size = socket.assigns.cell_size
+    column_index_target = Bubbles.get_index_from_coordinate(offsetX, cell_size)
+    row_index_target = Bubbles.get_index_from_coordinate(offsetY, cell_size)
 
-    generated_bubble = %{
-      id: id,
-      value: true,
-      column_index: column_number,
-      row_index: row_number
-    }
+    {updated_bubbles, updated_bubble} =
+      Bubbles.update_bubbles_grid(
+        socket.assigns.bubbles,
+        column_index_target,
+        row_index_target
+      )
 
-    socket =
-      stream_insert(socket, :bubbles_grid, generated_bubble, at: generated_bubble.id)
-
-    {:noreply, socket}
+    {:noreply,
+     socket
+     |> assign(:bubbles, updated_bubbles)
+     |> push_event("Canvas:update", %{
+       data: updated_bubble,
+       cell_size: cell_size
+     })}
   end
 
-  defp generate_bubbles(size) do
-    List.duplicate(false, size) |> Enum.map(fn _x -> List.duplicate(false, size) end)
-  end
+  def handle_event("change_grid_size", %{"amount" => amount}, socket) do
+    amount_integer = String.to_integer(amount)
 
-  defp generate_grid_from_bubbles_v2(bubbles) do
-    bubbles
-    |> Enum.with_index()
-    |> Enum.map(fn {value, index} ->
-      Enum.map(Enum.with_index(value), fn {val2, index2} ->
-        {val2, {index2, index}}
-      end)
-    end)
-    |> List.flatten()
-    |> Enum.map(fn {val, {col_index, row_index}} ->
-      %{
-        value: val,
-        column_index: col_index,
-        row_index: row_index
-      }
-    end)
-    |> Enum.with_index()
-    |> Enum.map(fn {val, index} -> Map.put(val, :id, index) end)
+    new_grid_size = socket.assigns.grid_size + amount_integer
+    new_cell_size = Bubbles.calculate_cell_size(socket.assigns.grid_dimension, new_grid_size)
+    new_bubbles = Bubbles.generate_bubbles_grid(new_grid_size, new_cell_size)
+
+    {:noreply,
+     socket
+     |> assign(:grid_size, new_grid_size)
+     |> assign(:cell_size, new_cell_size)
+     |> assign(:bubbles, new_bubbles)
+     |> push_event("Canvas:init", %{
+       data: new_bubbles,
+       cell_size: new_cell_size
+     })}
   end
 end
